87f04b30f96d0727a717938e4fb4f9efd6c09ea0
masterwebdev <masterwebdev@gmail.com>
2017-04-07 23:05:25 +0300

    fix

--- www/frames.js
+++ www/frames.js
@@ -1,10 +1,5 @@
-cordova.define("com.autovitalsinc.frames.frames", function(require, exports, module) {
-/*global cordova, module*/
-
 module.exports = {
     getframe: function (path, position, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Frames", "getframe", [path, position]);
     }
 };
-
-});
